57b17b38e94ed8b34f176c5f8ef8c3b167329877
Zigmas Petrauskas <zigmasdev@gmail.com>
2026-06-04 17:43:22 +0300

    dasdasdasdasf

---
+++ pamokos/js_datos/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/* gimtadienio apskai2iavimas
+// 1978-12-04
+let birthDateString = "1978-12-04";
+let birthDate = new Date(birthDateString);
+let miliseconds = birthDate.getTime();
+console.log(miliseconds);
+
+let date = new Date(281577600000);
+console.log(date);
+
+let now = new Date();
+let timeStamp = now.getTime();
+
+let age =(timeStamp - date)/1000/3600/24/365;
+console.log(age);
+
+*/
+
+let date = new Date();
